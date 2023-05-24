7efc73d92182d118090f220d82f2e9f06f958f21
Tamas Mezei <1107282+mezeitamas@users.noreply.github.com>
2023-05-24 12:02:01 +0200

    test: add layout test
    refs #8

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -4,14 +4,15 @@ const gatsby = jest.requireActual('gatsby');
 module.exports = {
     ...gatsby,
     graphql: jest.fn(),
-    Link: jest.fn().mockImplementation(
-        // these props are invalid for an `a` tag
-        ({ activeClassName, activeStyle, getProps, innerRef, partiallyActive, ref, replace, to, ...rest }) =>
-            React.createElement('a', {
-                ...rest,
-                href: to
-            })
-    ),
+    Link: jest
+        .fn()
+        .mockImplementation(
+            ({ activeClassName, activeStyle, getProps, innerRef, partiallyActive, ref, replace, to, ...rest }) =>
+                React.createElement('a', {
+                    ...rest,
+                    href: to
+                })
+        ),
     Slice: jest.fn().mockImplementation(({ alias, ...rest }) =>
         React.createElement('div', {
             ...rest,
