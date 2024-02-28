2c1f13951d54d433e19a43c38227772202a74a92
Tamas Mezei <1107282+mezeitamas@users.noreply.github.com>
2024-02-28 18:50:10 +0000

    chore: update dependencies

--- infra/aws/versions.tf
+++ infra/aws/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.67.0"
+      version = "~> 5.38.0"
     }
   }
 
-  required_version = ">= 1.4.0"
+  required_version = ">= 1.7.4"
 }
 
 provider "aws" {
