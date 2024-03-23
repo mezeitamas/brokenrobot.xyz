cc2029633ff9498436c19ccc5f8e5dc620312781
Tamas Mezei <1107282+mezeitamas@users.noreply.github.com>
2024-03-23 21:03:53 +0000

    chore: change variable definition

--- infra/aws/variables.tf
+++ infra/aws/variables.tf
@@ -2,5 +2,5 @@ variable "website_alarms_endpoints" {
   description = "The endpoints for the alarms SNS topic subscription"
   type        = list(string)
   nullable    = false
-  sensitive   = true
+  sensitive   = false
 }
