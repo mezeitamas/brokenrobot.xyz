a24c1cad6f453071ad341e893e5bd31cb13cefbb
Tamas Mezei <1107282+mezeitamas@users.noreply.github.com>
2024-04-27 18:34:29 +0000

    chore: change to non nullable

--- infra/aws/modules/simple-static-website/cloudfront-website/variables.tf
+++ infra/aws/modules/simple-static-website/cloudfront-website/variables.tf
@@ -27,7 +27,7 @@ variable "aws_cloudfront_function_viewer_request_arn" {
 variable "content_security_policy" {
   type        = string
   default     = "default-src 'none'; child-src 'none'; connect-src 'self'; font-src 'self'; frame-src 'none'; img-src 'self' 'unsafe-inline'; manifest-src 'none'; media-src 'none'; object-src 'none'; script-src 'self' 'unsafe-inline'; script-src-attr 'self'; script-src-elem 'self' 'unsafe-inline'; style-src 'self' 'unsafe-inline'; style-src-attr 'self' 'unsafe-inline'; style-src-elem 'self' 'unsafe-inline'; worker-src 'none'; base-uri 'none'; form-action 'none'; frame-ancestors 'none';"
-  nullable    = true
+  nullable    = false
 }
 
 variable "aliases" {
