092d0d48ea163421bb9708c4215eb791a694fcca
Tamas Mezei <1107282+mezeitamas@users.noreply.github.com>
2024-07-10 14:20:33 +0200

    chore: change pixel ratio

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     snapshotPathTemplate: '{testDir}/__screenshots__/{testFilePath}/{arg}{-projectName}{ext}',
     expect: {
         toHaveScreenshot: {
-            maxDiffPixelRatio: 0.01,
+            maxDiffPixelRatio: 0.075,
             stylePath: './tests/screenshot.css'
         }
     },
