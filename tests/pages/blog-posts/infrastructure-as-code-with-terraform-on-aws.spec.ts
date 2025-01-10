6e10945a7a0f2b4fd9bd63f28a9820ba01b65485
Tamas Mezei <1107282+mezeitamas@users.noreply.github.com>
2025-01-10 12:09:47 +0100

    post: change title and url, snapshot tests and small fixes

--- tests/pages/blog-posts/infrastructure-as-code-with-terraform-on-aws.spec.ts
+++ tests/pages/blog-posts/infrastructure-as-code-with-terraform-on-aws.spec.ts
@@ -1,13 +1,13 @@
 import AxeBuilder from '@axe-core/playwright';
 import { test, expect } from '@playwright/test';
 
-test.describe('Post: Infrastructure as Code with Terraform', () => {
+test.describe('Post: Infrastructure as Code with Terraform on AWS', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('./blog/infrastructure-as-code-with-terraform');
+        await page.goto('./blog/infrastructure-as-code-with-terraform-on-aws');
     });
 
     test('should have a title', async ({ page }) => {
-        await expect(page).toHaveTitle(/Infrastructure as Code with Terraform/);
+        await expect(page).toHaveTitle(/Infrastructure as Code with Terraform on AWS/);
     });
 
     test('should not have any automatically detectable accessibility issues', async ({ page }) => {
