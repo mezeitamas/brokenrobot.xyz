919983f7f2fad65085220a1a7b414a074ff0ce4f
Tamas Mezei <1107282+mezeitamas@users.noreply.github.com>
2025-10-27 10:31:11 +0100

    fix: rss feed generation

--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,12 +1,8 @@
 import rss from '@astrojs/rss';
 import type { APIRoute } from 'astro';
 import { getCollection } from 'astro:content';
-import MarkdownIt from 'markdown-it';
-import sanitizeHtml from 'sanitize-html';
 
 import { SITE_METADATA } from '../consts';
-
-const parser = new MarkdownIt();
 
 export const GET: APIRoute = async ({ site }) => {
     const posts = await getCollection('blog');
@@ -19,7 +15,6 @@ export const GET: APIRoute = async ({ site }) => {
             title: post.data.title,
             pubDate: post.data.publishDate,
             description: post.data.excerpt,
-            content: sanitizeHtml(parser.render(post.body)),
             link: `/blog/${post.slug}`
         })),
         trailingSlash: false
